dba57144bd3e96ab6cd14d9059e6670aab7565f1
Avi Samloff <avi.samloff@gmail.com>
2021-01-18 21:31:40 -0600

    Update priest-basic-heal.js

--- priest-basic-heal.js
+++ priest-basic-heal.js
@@ -1,34 +1,70 @@
 // VARS
-const _focus_character = "aviswings";
+const _focus_character = "avitanks";
 const _respawn_on_death = true;
+const _minimum_healing_threshold = character.attack * 2;
 
 // CODE
-const getNeedsHealingFromParty = (healingThreshold = character.attack * 2) => {
-	const needsHealing = [];
-			
-	for (const name in get_party()) {
-		const ally = get_player(name);
-		if (!ally) continue;
+const getNeedsHealingFromParty =
+	(healingThreshold = _minimum_healing_threshold) => {
+		const needsHealing = [];
+
+		for (const name in get_party()) {
+			const ally = get_player(name);
+			if (!ally) continue;
+
+			const missingLife = ally.max_hp - ally.hp;
+			if (missingLife < healingThreshold) continue;
+
+			needsHealing.push(ally);
+		}
+
+		return needsHealing;
+	};
+
+const manage_hp_and_mp = () => {
+	if (mssince(new Date(get("last_potion"))) < 600) return;
+	
+	let used = false;
 		
-		const missingLife = ally.max_hp - ally.hp;
-		if (missingLife < healingThreshold) continue;
-		
-		needsHealing.push(ally);
+	if (character.hp < character.max_hp * 0.4) {
+		if (!is_on_cooldown("use_hp")) {
+			log("Drinking HP Potion!");
+			use_skill("use_hp");
+			used = true;
+		}
+	} else if (character.hp < character.max_hp) {
+		if (!is_on_cooldown("regen_hp")) {
+			use_skill("regen_hp");
+			used = true;
+		}
 	}
 	
-	return needsHealing;
-}
+	if (character.mp < character.max_mp * 0.5) {
+		if (!is_on_cooldown("use_mp")) {
+			log("Drinking MP Potion!");
+			use_skill('use_mp'); 
+			used = true;
+		}
+	} else if (character.mp < character.max_mp) {
+		if (!is_on_cooldown("regen_mp")) {
+			use_skill("regen_mp");
+			used = true;
+		}
+	}
+	
+	if (used) set("last_potion", new Date().getTime());
+};
 
 setInterval(() => {
 	if (character.rip) {
 		if (_respawn_on_death) {
-			respawn()
+			respawn();
 		}
 		
 		return;
 	}
 	
-	use_hp_or_mp();
+	manage_hp_and_mp();
 	loot();
 
 	if (is_moving(character)) return;
@@ -43,7 +79,7 @@ setInterval(() => {
 		if (!tankTarget || !is_monster(tankTarget)) {
 			const inRange = parent.distance(character, tank) > 100;
 			if (!inRange) {
-				move(
+				xmove(
 					character.x + (tank.x - character.x) / 2,
 					character.y + (tank.y - character.y) / 2
 				);
@@ -74,7 +110,7 @@ setInterval(() => {
 			
 			if (inRange && can_heal(ally)) {
 				log(`Healing ${ally.name}`);
-				set_message("Healing")
+				set_message("Healing");
 				heal(ally);
 			}
 		});
